2c44bc644d87a5eaca6328e5ec187dcbce2bfbaf
Surya_T <71425657+suryaT1@users.noreply.github.com>
2025-03-29 11:19:16 +0530

    Update ami.tf

--- terraformfiles/datasource/ami.tf
+++ terraformfiles/datasource/ami.tf
@@ -1,7 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["amazon"]  # Canonical's AWS account ID
