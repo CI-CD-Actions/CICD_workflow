0ce4539114bf6fedcdbbbb20e916623bcd70d36c
Surya_T <71425657+suryaT1@users.noreply.github.com>
2025-03-29 15:59:39 +0530

    Update outputs.tf

--- terraformfiles/outputs.tf
+++ terraformfiles/outputs.tf
@@ -1,11 +1,3 @@
-output "ec2_public_ips" {
-  value = aws_instance.my_ec2_system[*].public_ip
-}
-
-output "ec2_private_ips" {
+output "private_ips" {
   value = aws_instance.my_ec2_system[*].private_ip
-}
-
-output "ec2_instance_ids" {
-  value = aws_instance.my_ec2_system[*].id
 }
