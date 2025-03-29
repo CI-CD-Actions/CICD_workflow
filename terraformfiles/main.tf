2ae09bc5cc339a8f2b19c990263ea06f5bb7bd5f
Surya_T <71425657+suryaT1@users.noreply.github.com>
2025-03-29 11:03:40 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -18,7 +18,8 @@ resource "aws_instance" "my_ec2_system" {
   ami             = module.aws_ami.ami_id
   instance_type   = "t2.micro"
   key_name        = "key-001501e8cc53ade44"
-
+  vpc_security_group_ids = [ module.module_vpc.cicd_sg ] #security group assigned
+  subnet_id = module.module_vpc.cicd_subnet  # updated subnet group
   tags = {
     Name = "cicd-ec2-machine"
   }
