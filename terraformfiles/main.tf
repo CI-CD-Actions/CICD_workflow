caf14c57ef86b712d19d7178095e3e7a1a4d58f3
Surya_T <71425657+suryaT1@users.noreply.github.com>
2025-03-29 14:22:45 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -20,7 +20,12 @@ resource "aws_instance" "my_ec2_system" {
   key_name        = "CICD_Server"
   vpc_security_group_ids = [ module.module_vpc.cicd_sg ] #security group assigned
   subnet_id = module.module_vpc.cicd_subnet  # updated subnet group
+  count=2
   tags = {
     Name = "cicd-ec2-machine"
   }
+}
+
+output "private_ips" {
+  value = aws_instance.my_ec2_system[*].private_ip
 }
