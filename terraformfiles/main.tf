b04bf2457b916a29cb6260f4cb17aa68d8a69f60
Surya_T <71425657+suryaT1@users.noreply.github.com>
2025-03-29 10:49:00 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -8,7 +8,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 resource "aws_instance" "my_ec2_system" {
-  ami             = "ami-0015f25daf18f6af4"
+  ami             = "ami-084568db4383264d4"
   instance_type   = "t2.micro"
   key_name        = "key-001501e8cc53ade44"
 
